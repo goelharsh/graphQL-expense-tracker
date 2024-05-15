a66467a9dd3e43b86621c42886c0faa8d7e27046
goelharsh <harshgoel18122001@gmail.com>
2024-05-16 05:11:23 +0530

    login signup functionality done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,16 +4,25 @@ import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom"
 import GridBackground from './components/ui/GridBackgroun.jsx'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { MdSignalCellularConnectedNoInternet4Bar } from 'react-icons/md'
 
-
-
+const client = new ApolloClient({
+  // TODO: UODATE THE URI ON PRODUCTION 
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(), // Apollo client uses to cahe query results after fetching them.
+  credentials:"include",  //this tells Apollo Client to send cookies along with every request to the server
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <GridBackground>
+        <ApolloProvider client={client}>
           <App />
+        </ApolloProvider>
       </GridBackground>
     </BrowserRouter>
   </React.StrictMode>,
 )
+ 
